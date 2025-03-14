1c7e1fb40f52bedadb42d84849bc0172e13f5812
Strider221B <somesh.chatterjee@gmail.com>
2025-03-14 15:48:42 +0530

    miscellaneous

---
+++ 02_cpp/06_misc/01_struct_pointers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Person {
+  int age;
+  char* name;
+};
+
+
+int main() {
+  struct Person person1;
+  person1.age = 25;
+  person1.name = "elliot";
+
+  printf("age: %d\tname: %s\n", person1.age, person1.name);
+
+  struct Person* person2 = malloc(sizeof(struct Person));
+  // Same as: struct Person* person2 = (struct Person*)malloc(sizeof(struct Person));
+  person2->age = 20;
+  person2->name = "not elliot";
+
+  printf("age: %d\tname: %s\n", person2->age, person2->name);
+  return 0;
+}
+
+// Output:
+// age: 25 name: elliot
+// age: 20 name: not elliot
